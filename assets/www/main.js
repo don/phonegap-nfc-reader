a0bf6d0e0b68f24f047c2668d59ba6a5d2a0a986
Don Coleman <dcoleman@chariotsolutions.com>
2011-07-13 11:53:45 -0400

    move js and css out of html

--- assets/www/main.js
+++ assets/www/main.js
@@ -1,35 +1,86 @@
-/*global NdefPlugin, Ndef */
+/*global Ndef */
+var tagMimeType = "text/pg";
 
-function onNfc(nfcEvent) {
-  console.log(JSON.stringify(nfcEvent.tagData));
-  // ignore what's on the tag for now, just overwrite
-  
-  var mimeType = document.forms[0].elements["mimeType"].value,
-    payload = document.forms[0].elements["payload"].value,
-    record = Ndef.mimeMediaRecord(mimeType, Ndef.stringToBytes(payload));
+function template(record) {
+    var recordType = Ndef.bytesToString(record.type),
+    payload;
 
-  navigator.nfc.writeTag(
-        [record], 
-        function () { 
-            navigator.notification.vibrate(100);
-        }, function () {
-            alert("Failed to write message to tag.");
-        });   
+    if (recordType === "T") {
+        var langCodeLength = record.payload[0],
+        text = record.payload.slice((1 + langCodeLength), record.payload.length);
+
+        payload = Ndef.bytesToString(text);
+
+    } else if (recordType === "U") {
+        var url = Ndef.bytesToString(record.payload);
+        payload = "<a href='" + url + "'>" + url + "<\/a>";
+
+    } else {
+        payload = Ndef.bytesToString(record.payload);
+
+    }
+
+    return ("record type: <b>" + recordType + "<\/b>" +
+    "<br/>" + payload
+    );
 }
 
-var ready = function () {
-  
-  function win() {
-    console.log("Listening for NDEF tags");
-  }
-  
-  function fail() {
-    alert('Failed to register NFC Listener');
-  }
-  
-  navigator.nfc.addNdefListener(onNfc, win, fail);          
+function clearScreen() {
+    document.getElementById("tagContents").innerHTML = "";
+}
+
+function showInstructions() {
+    document.getElementById("tagContents").innerHTML =
+    "<div id='instructions'>" +
+    " <p>Scan a tag to begin.<\/p>" +
+    " <p>Expecting Mime Media Tags with a type of " + tagMimeType + ".<\/p>" +
+    " <p>Use the menu button to clear the screen.<\/p>" +
+    "<\/div>";
+}
+
+function myNfcListener(nfcEvent) {
+    console.log(JSON.stringify(nfcEvent.tagData));
+    clearScreen();
+
+    var records = nfcEvent.tagData,
+    display = document.getElementById("tagContents");
+    display.appendChild(document.createTextNode(
+    "Scanned a NDEF tag with " + records.length + " record" + ((records.length === 1) ? "": "s"))
+    );
+
+    for (var i = 0; i < records.length; i++) {
+
+        var record = records[i],
+        p = document.createElement('p');
+
+        p.innerHTML = template(record);
+        display.appendChild(p);
+    }
+    navigator.notification.vibrate(100);
+}
+
+var ready = function() {
+
+    function win() {
+        console.log("Listening for tags with mime type " + tagMimeType);
+    }
+
+    function fail() {
+        alert('Failed to register mime type ' + tagMimeType + ' with NFC');
+    }
+
+    navigator.nfc.addMimeTypeListener(tagMimeType, myNfcListener, win, fail);
+    navigator.nfc.addNdefListener(function() {
+        alert("This is an NDEF tag but doesn't have the mime type (" + tagMimeType + ") we're looking for.");
+    });
+    navigator.nfc.addNdefFormattableListener(function() {
+        alert("This tag is formattable");
+    });
+
+    showInstructions();
+
 };
 
-// deviceready is being called before the plugins finish initializing
-// add setTimeout as a kludge until the real problem is fixed
+document.addEventListener("menubutton", showInstructions, false);
+
 document.addEventListener('deviceready', ready, false);
