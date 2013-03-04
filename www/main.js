1543adbe769f0681f36f993aeb6f319911b4d9e2
Don Coleman <dcoleman@chariotsolutions.com>
2013-03-03 22:32:43 -0500

    add javascript app object

--- www/main.js
+++ www/main.js
@@ -1,169 +1,161 @@
-// TODO wrap in app object
-
-function init() {
-    document.addEventListener('deviceready', ready, false);
-}
-
-function clearScreen() {
-    document.getElementById("tagContents").innerHTML = "";
-}
-
-// TODO move markup to index.html
-function showInstructions() {
-    document.getElementById("tagContents").innerHTML =
-    "<div id='instructions'>" +
-    " <p>Scan a tag to begin.<\/p>" +
-    " <p><\/p>" +    
-    "<\/div>";
-}
-
-function onNfc(nfcEvent) {
-    console.log(JSON.stringify(nfcEvent.tag));
-    clearScreen();
-
-    var tag = nfcEvent.tag,
-        source = document.getElementById('non-ndef-template').innerHTML,
-        template = Handlebars.compile(source);
-
-    tagContents.innerHTML = template(tag);
-
-    navigator.notification.vibrate(100);
-}
-
-function onNdef(nfcEvent) {
-    console.log(JSON.stringify(nfcEvent.tag));
-    clearScreen();
-
-    var tag = nfcEvent.tag;
-
-    // BB7 has different names, copy to Android names
-    if (tag.serialNumber) {
-        tag.id = tag.serialNumber;
-        tag.isWritable = !tag.isLocked;
-        tag.canMakeReadOnly = tag.isLockable;
-    }
-
-    // TODO compile template outside of listener
-    var source = document.getElementById('tag-template').innerHTML;
-    var template = Handlebars.compile(source);
+var app = {
+    initialize: function () {
+        this.bind();
+    },
+    bind: function () {
+        document.addEventListener('deviceready', this.deviceready, false);
+    },
+    deviceready: function () {
     
-    tagContents.innerHTML = template(tag);
-
-    navigator.notification.vibrate(100);
-}
-
-var ready = function() {
-
-    function failure(reason) {
-        navigator.notification.alert(reason, function() {}, "There was a problem");
-    }
-    
-    nfc.addNdefListener(
-        onNdef,
-        function() {
-            console.log("Listening for NDEF tags.");
-        },
-        failure
-    );
-    
-    if (device.platform == "Android") {
-
-        // Android reads non-NDEF tag. BlackBerry and Windows don't.
-        nfc.addTagDiscoveredListener(
-            onNfc,
-            function() {
-                console.log("Listening for non-NDEF tags.");
-            },
-            failure
-        );
-
-        // Android launches the app when tags with mime type text/pg are scanned
-        // because of an intent in AndroidManifest.xml.
-        // phonegap-nfc fires an ndef-mime event (as opposed to an ndef event)
-        // the code reuses the same onNfc handler
-        nfc.addMimeTypeListener(
-            'text/pg',
-            onNdef,
-            function() {
-                console.log("Listening for NDEF mime tags with type text/pg.");
-            },
-            failure
-        );
-
-    }
-
-    Handlebars.registerHelper('bytesToString', function(byteArray) { 
-        return nfc.bytesToString(byteArray);  // TODO nfc util
-    });
-
-    Handlebars.registerHelper('bytesToHexString', function(byteArray) {
-        return nfc.bytesToHexString(byteArray);  // TODO nfc util
-    });
-
-    // useful for boolean
-    Handlebars.registerHelper('toString', function(value) {  
-        return String(value);  
-    });
-
-    Handlebars.registerHelper('tnfToString', function(tnf) {  
-        return tnfToString(tnf);  
-    });
-
-    Handlebars.registerHelper('decodePayload', function(record) {
-
-        var recordType = nfc.bytesToString(record.type),
-            payload;
-
-        // TODO extract this out to decoders that live in NFC code
-        // TODO add a method to ndefRecord so the helper 
-        // TODO doesn't need to do this
-
-        if (recordType === "T") {
-            var langCodeLength = record.payload[0],
-            text = record.payload.slice((1 + langCodeLength), record.payload.length);
-            payload = nfc.bytesToString(text);
-
-        } else if (recordType === "U") {
-            var identifierCode = record.payload.shift(),
-            uri =  nfc.bytesToString(record.payload);
-
-            if (identifierCode !== 0) {
-                // TODO decode based on URI Record Type Definition
-                console.log("WARNING: uri needs to be decoded");
-            }
-            //payload = "<a href='" + uri + "'>" + uri + "<\/a>";
-            payload = uri;
-
-        } else {
-
-            // kludge assume we can treat as String
-            payload = nfc.bytesToString(record.payload); 
+        function failure(reason) {
+            navigator.notification.alert(reason, function() {}, "There was a problem");
         }
 
-        return payload;
-    });
+        nfc.addNdefListener(
+            app.onNdef,
+            function() {
+                console.log("Listening for NDEF tags.");
+            },
+            failure
+        );
 
+        if (device.platform == "Android") {
 
-    showInstructions();
-    
+            // Android reads non-NDEF tag. BlackBerry and Windows don't.
+            nfc.addTagDiscoveredListener(
+                app.onNfc,
+                function() {
+                    console.log("Listening for non-NDEF tags.");
+                },
+                failure
+            );
+
+            // Android launches the app when tags with mime type text/pg are scanned
+            // because of an intent in AndroidManifest.xml.
+            // phonegap-nfc fires an ndef-mime event (as opposed to an ndef event)
+            // the code reuses the same onNfc handler
+            nfc.addMimeTypeListener(
+                'text/pg',
+                app.onNdef,
+                function() {
+                    console.log("Listening for NDEF mime tags with type text/pg.");
+                },
+                failure
+            );
+        }
+
+        app.compileTemplates();
+        app.addTemplateHelpers();
+        app.showInstructions();
+    },
+    onNfc: function (nfcEvent) {
+        
+        console.log(JSON.stringify(nfcEvent.tag));
+        app.clearScreen();
+
+        tagContents.innerHTML = app.nonNdefTagTemplate(tag);    
+        navigator.notification.vibrate(100);        
+    },
+    onNdef: function (nfcEvent) {
+        
+        console.log(JSON.stringify(nfcEvent.tag));
+        app.clearScreen();
+
+        var tag = nfcEvent.tag;
+
+        // BB7 has different names, copy to Android names
+        if (tag.serialNumber) {
+            tag.id = tag.serialNumber;
+            tag.isWritable = !tag.isLocked;
+            tag.canMakeReadOnly = tag.isLockable;
+        }
+
+        tagContents.innerHTML = app.tagTemplate(tag);
+
+        navigator.notification.vibrate(100);        
+    },
+    clearScreen: function () {
+        
+        tagContents.innerHTML = "";
+        
+    },
+    showInstructions: function () {
+
+        var hidden = document.getElementsByClassName('hidden');
+        if (hidden && hidden.length) {
+            hidden[0].className = 'instructions';
+        }
+        
+    },
+    compileTemplates: function () {
+
+        var source;
+                    
+        source = document.getElementById('non-ndef-template').innerHTML;
+        app.nonNdefTagTemplate = Handlebars.compile(source);
+
+        source = document.getElementById('tag-template').innerHTML;
+        app.tagTemplate = Handlebars.compile(source);
+        
+    },
+    addTemplateHelpers: function () {
+        
+        Handlebars.registerHelper('bytesToString', function(byteArray) { 
+            return nfc.bytesToString(byteArray);
+        });
+
+        Handlebars.registerHelper('bytesToHexString', function(byteArray) {
+            return nfc.bytesToHexString(byteArray); 
+        });
+
+        // useful for boolean
+        Handlebars.registerHelper('toString', function(value) {  
+            return String(value);  
+        });
+
+        Handlebars.registerHelper('tnfToString', function(tnf) {  
+            return tnfToString(tnf);  
+        });
+
+        Handlebars.registerHelper('decodePayload', function(record) {
+            return decodePayload(record);
+        });     
+    }
 };
 
+// ideally some form of this can move to phonegap-nfc util
+function decodePayload(record) {
+    var recordType = nfc.bytesToString(record.type),
+        payload;
 
-// TODO - process the message before sending to template??
-function convertData() {
+    // TODO extract this out to decoders that live in NFC code
+    // TODO add a method to ndefRecord so the helper 
+    // TODO doesn't need to do this
 
-    // tag
-    // add id as hex string (keep binary version)
-    // convert boolean to string (keep boolean?)
+    if (recordType === "T") {
+        var langCodeLength = record.payload[0],
+        text = record.payload.slice((1 + langCodeLength), record.payload.length);
+        payload = nfc.bytesToString(text);
 
-    // for each record in the ndef message
-    // record.tnfString = tnfToString(record.tnf);
-    // record.typeString = nfc.bytesToString(record.type);
-    // record.payloadString = nfc.decodePayload(record);
-    //     TODO can people add their own decode?
-    // record.idString = nfc.bytesToString(record.id);
+    } else if (recordType === "U") {
+        var identifierCode = record.payload.shift(),
+        uri =  nfc.bytesToString(record.payload);
+
+        if (identifierCode !== 0) {
+            // TODO decode based on URI Record Type Definition
+            console.log("WARNING: uri needs to be decoded");
+        }
+        //payload = "<a href='" + uri + "'>" + uri + "<\/a>";
+        payload = uri;
+
+    } else {
+
+        // kludge assume we can treat as String
+        payload = nfc.bytesToString(record.payload); 
+    }
+
+    return payload;
 }
-
 
 // TODO move to phonegap-nfc util
 function tnfToString(tnf) {
