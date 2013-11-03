3b48bfcc152fb01ddaadc2318e033f2bd56a75eb
Don Coleman <dcoleman@chariotsolutions.com>
2013-11-02 20:14:32 -0400

    Cordova 3.1

--- platforms/android/src/com/megster/nfc/reader/NfcReader.java
+++ platforms/android/src/com/megster/nfc/reader/NfcReader.java
@@ -22,12 +22,13 @@ package com.megster.nfc.reader;
 import android.os.Bundle;
 import org.apache.cordova.*;
 
-public class NfcReader extends DroidGap
+public class NfcReader extends CordovaActivity 
 {
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        super.init();
         // Set by <content src="index.html" /> in config.xml
         super.loadUrl(Config.getStartUrl());
         //super.loadUrl("file:///android_asset/www/index.html")
